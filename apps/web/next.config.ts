e4ca8488b3c1f5df98ee276e90381ed288b6a6a5
Nebil <a0911991679@gmail.com>
2026-04-30 15:57:27 +0300

    feat: Project setup

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@campus-hub-component-based-project/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
